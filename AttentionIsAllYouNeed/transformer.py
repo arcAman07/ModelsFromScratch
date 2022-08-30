c33c7301d860f2f29237f3d2fd4e534554e883ad
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-08-30 21:33:53 +0530

    added archictecture

--- AttentionIsAllYouNeed/transformer.py
+++ AttentionIsAllYouNeed/transformer.py
@@ -277,5 +277,7 @@ if __name__ == "__main__":
     model = Transformer(src_vocab_size, trg_vocab_size, src_pad_idx, trg_pad_idx, device=device).to(
         device
     )
-    out = model(x, trg[:, :-1])
+    out = model(x, trg)
+    # print(trg[:, :-1])
+    print(out)
     print(out.shape)
--- U-Net/model.py
+++ U-Net/model.py
@@ -1,3 +1,11 @@
 import torch
 import torch.nn as nn
 
+class UNet(nn.Module):
+  def __init__(self, in_channels, out_channels):
+    super(UNet, self).__init__()
+    self.in_channels = in_channels
+    self.out_channels = out_channels
+    self.downsample_block_1 = nn.Sequential(
+      nn.Conv2d(in_channels, 64, kernel_size= 3, )
+    )
