4c330453575c374128947e558a94e4324ca86a5c
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-09-01 17:36:13 +0530

    added LeNet model

--- LeNet/train.py
+++ LeNet/train.py
@@ -1,13 +1,16 @@
+import torchvision
+from model import LeNet
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
 from torchvision import datasets
+from torchvision import transforms
 from torchvision.transforms import ToTensor
+from torchvision.transforms import Resize
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision.transforms import ToTensor, Normalize, Compose
-from model import LeNet
 from torch.utils.data.dataloader import DataLoader
 
 batch_size=128
@@ -16,18 +19,28 @@ train_data = datasets.FashionMNIST(
     root="data",
     train=True,
     download=True,
-    transform=ToTensor()
+    transform=transforms.Compose(
+        [
+        Resize(size = (32,32)),
+        ToTensor(),
+        ]
+    )
 )
 
 test_data = datasets.FashionMNIST(
     root="data",
     train=False,
     download=True,
-    transform=ToTensor()
+    transform=transforms.Compose(
+        [
+        Resize(size = (32,32)),
+        ToTensor(),
+        ]
+    )
 )
 
-train_dl = DataLoader(train_data, batch_size, shuffle=True, num_workers=4, pin_memory=True)
-val_dl = DataLoader(test_data, batch_size*2, num_workers=4, pin_memory=True)
+train_dl = DataLoader(train_data, batch_size, shuffle=True)
+val_dl = DataLoader(test_data, batch_size*2)
         
 def accuracy(outputs, labels):
     _, preds = torch.max(outputs, dim=1)
